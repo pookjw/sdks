e60c60144a555329e798ea808a265d8cf56bce53
Jinwoo Kim <kidjinwoo@me.com>
2022-03-06 04:04:02 +0900

    removed obsolete sdks

--- AppleTVOS11.4.sdk/System/Library/Frameworks/CoreData.framework/Headers/NSCoreDataCoreSpotlightDelegate.h
+++
@@ -1,7 +0,0 @@
-/*
-    NSCoreDataCoreSpotlightDelegate.h
-    Core Data
-    Copyright (c) 2017, Apple Inc.
-    All rights reserved.
-*/
-
--- AppleTVOS12.4.sdk/System/Library/Frameworks/CoreData.framework/Headers/NSCoreDataCoreSpotlightDelegate.h
+++
@@ -1,7 +0,0 @@
-/*
-    NSCoreDataCoreSpotlightDelegate.h
-    Core Data
-    Copyright (c) 2017-2018, Apple Inc.
-    All rights reserved.
-*/
-
